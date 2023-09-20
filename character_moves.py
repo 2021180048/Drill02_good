89098c03a81a307a3751d1d69170f77b2d976226
minkeuncho <minkeun123@naver.com>
2023-09-20 18:31:09 +0900

    전체완성

--- character_moves.py
+++ character_moves.py
@@ -16,8 +16,8 @@ def run_circle():
     
     cx = 800 / 2
     cy = 600 / 2
-    r = 200
-    for deg in range(0, 360, 5):
+    r = 210
+    for deg in range(270, -90-1, -5):
         x = cx + r * math.cos(deg /360 * 2 * math.pi)
         y = cy + r * math.sin(deg /360 * 2 * math.pi)
         render_all(x,y)
@@ -25,24 +25,21 @@ def run_circle():
 def run_rectangle():
     print('rectangle')
     #bottom line
-    #for x in range(50,750+1,10):
-        #render_all(x,90)
-    
-    #top line
-    #for x in range(750,50-1,-10):
-    #   render_all(x,550)
-
+    for x in range(50,750+1,10):
+        render_all(x,90)
     #right line
-    #for y in range(90,550+1,10):
-    #   render_all(750,y)
-
+    for y in range(90,550+1,10):
+       render_all(750,y)
+    #top line
+    for x in range(750,50-1,-10):
+       render_all(x,550)
     #left line
     for y in range(550,90-1,-10):
         render_all(50,y)
 
 while (True):
     run_rectangle()
-    #run_circle()
+    run_circle()
     break
 
 
